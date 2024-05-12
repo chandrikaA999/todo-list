3d3cc2cc1427982abef04c0fd034bfd0e29f992a
chandrika <adabalachandrika999@gmail.com>
2024-05-12 21:44:49 +0530

    todo with strike and remove

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./styles.css";
+
+import Header from './components/Header/Header';
+import ToDoList from './components/ToDoList/ToDoList'
+
+
+function App() {
+
+  return (
+
+    <div className="App">
+      <Header />
+      <ToDoList />
+    </div>
+  );
+}
+
+export default App;
